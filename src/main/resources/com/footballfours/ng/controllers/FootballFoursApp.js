b9155442c08be2c65223b2a6a4e39492a2ab147e
Robert Williams <rwilliams@qvalent.com>
2016-06-10 09:41:44 +1000

    Cleaning up code

--- src/main/resources/com/footballfours/ng/controllers/FootballFoursApp.js
+++ src/main/resources/com/footballfours/ng/controllers/FootballFoursApp.js
@@ -1,42 +1,42 @@
 var FootballFoursApp = angular.module("FootballFoursApp", [ 'ngRoute' ]);
 
 var httpConfig = {
-	headers : {
-		'Accept' : 'application/json;odata=verbose'
-	}
+    headers : {
+        'Accept' : 'application/json;odata=verbose'
+    }
 };
 
 FootballFoursApp.config(function($routeProvider) {
-	$routeProvider.when('/', {}).when('/rounds', {
-		templateUrl : 'pages/rounds.html',
-		controller : 'roundsController',
-		pageHeading : 'fixtures'
+    $routeProvider.when('/', {}).when('/rounds', {
+        templateUrl : 'pages/rounds.html',
+        controller : 'roundsController',
+        pageHeading : 'fixtures'
 
-	}).when('/tables', {
-		templateUrl : 'pages/tables.html',
-		controller : 'tablesController',
-		pageHeading : 'tables'
+    }).when('/tables', {
+        templateUrl : 'pages/tables.html',
+        controller : 'tablesController',
+        pageHeading : 'tables'
 
-	}).when('/rules', {
-		templateUrl : 'pages/rulessummary.html',
-		pageHeading : 'rules'
+    }).when('/rules', {
+        templateUrl : 'pages/rulessummary.html',
+        pageHeading : 'rules'
 
-	}).otherwise( {
-		templateUrl : '/404.html',
-	});
-	;
+    }).otherwise({
+        templateUrl : '/404.html',
+    });
+    ;
 });
 
 FootballFoursApp.run([ '$rootScope', function($rootScope) {
-	$rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
-		$rootScope.pageHeading = current.$$route.pageHeading;
-	});
+    $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+        $rootScope.pageHeading = current.$$route.pageHeading;
+    });
 } ]);
 
 FootballFoursApp.filter('prettyJSON', function() {
-	function prettyPrintJson(json) {
-		return JSON ? JSON.stringify(json, null, '  ')
-				: 'your browser doesnt support JSON so cant pretty print';
-	}
-	return prettyPrintJson;
+    function prettyPrintJson(json) {
+        return JSON ? JSON.stringify(json, null, '  ')
+                : 'your browser doesnt support JSON so cant pretty print';
+    }
+    return prettyPrintJson;
 });
--- src/main/resources/com/footballfours/ng/controllers/UserController.js
+++ src/main/resources/com/footballfours/ng/controllers/UserController.js
@@ -1,15 +1,15 @@
 FootballFoursApp.controller('userController', function($scope, $http) {
 
-	$scope.login = function() {
-		$http.post("login", $scope.newUser, httpConfig).success(
-				function(response) {
-					$scope.user = response.user;
-				});
-	}
+    $scope.login = function() {
+        $http.post("login", $scope.newUser, httpConfig).success(
+                function(response) {
+                    $scope.user = response.user;
+                });
+    }
 
-	$scope.logout = function() {
-		$http.get("logout", httpConfig).success(function(response) {
-			$scope.user = response.user;
-		});
-	}
+    $scope.logout = function() {
+        $http.get("logout", httpConfig).success(function(response) {
+            $scope.user = response.user;
+        });
+    }
 });
